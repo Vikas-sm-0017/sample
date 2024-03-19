8cd82ae0e138e488d86541d7c23f43ceee06a269
Vikas-sm-0017 <vikasvikki4803@gmail.com>
2024-03-19 16:58:48 +0530

    ver 4.0

---
+++ name.c
@@ -0,0 +1,8 @@
+#include "add.h"
+
+int main()
+{
+int result = add(5,3);
+printf("result: %d\n", result);
+return 0;
+}
---
+++ name.h
@@ -0,0 +1,6 @@
+#include<stdio.h>
+//sample
+int add(int a,int b)
+{
+return a + b;
+}
